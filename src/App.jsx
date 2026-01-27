9e6d60a308f70a380654bb8f6813ef9131e31c6c
Ali <aliazadigithub@gmail.com>
2026-01-27 13:44:27 +0330

    Create and initialize project files

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { useRoutes } from "react-router-dom"
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Notfound from "./Pages/Notfound";
+
+const routes = [
+  { path: "/", element: <Home /> },
+  // { path: "/Products", element: <Shop /> },
+  { path: "/Login", element: <Login /> },
+  // { path: "/Profile", element: <Profile /> },
+  { path: "*", element: <Notfound /> },
+  // { path: "/product/:id", element: <Details /> },
+];
+function App() {
+  const router = useRoutes(routes);
+  return (
+    <>
+      <div dir="rtl">
+      </div>
+
+      {router}
+    </>
+  );
+}
+
+export default App;
