9d2b0bac2dc5191f7cdab29ec6454a69761008ea
Ali <aliazadigithub@gmail.com>
2026-02-01 23:36:17 +0330

    Add : Zod installation + login schema validation

--- Frontend-React/src/Pages/Login.jsx
+++ Frontend-React/src/Pages/Login.jsx
@@ -1,58 +1,28 @@
 import React, { useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
+import loginSchema from "../../Validators/Login";
 
 const Login = () => {
   const [username, setUsername] = useState("");
-  const [usernameerror, setUsernameerror] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
 
   const handleLogin = () => {
-    setTimeout(()=>{
-
-      toast.error("وارد کردن فیلد های زیر الزامی است.", { duration: 3000  });
-    },2000)
+    const newUser = { username, password };
+    const result = loginSchema.safeParse(newUser);
+    // console.log(result.error.issues[0].message);
+    setTimeout(() => {
+      if (result.success) {
+        return toast.success("ورود با موفقیت انجام شد.", { duration: 3000 });
+      }
+      return toast.error(result.error.issues[0].message, { duration: 3000 });
+    }, 2000);
     toast.loading("درحال برسی اطلاعات", { duration: 2000 });
-  };
-  // const handleLogin = async () => {
-  //   setError(""); // پاک کردن خطا قبل از ارسال
-  //   try {
-  //     const res = await axios.post("http://127.0.0.1:8000/api/users/login/", {
-  //       username,
-  //       password,
-  //     });
-
-  //     // ذخیره توکن‌ها در localStorage
-  //     localStorage.setItem("access_token", res.data.access);
-  //     localStorage.setItem("refresh_token", res.data.refresh);
-
-  //     alert(`ورود موفق! خوش آمدید ${res.data.username}`);
-
-  //     // اینجا می‌توان به صفحه دیگر ریدایرکت کرد
-  //     // مثلا: navigate("/dashboard")
-
-  //   } catch (err) {
-  //     if (err.response) {
-  //       // نمایش پیام خطا از بک‌اند
-  //       const detail = err.response.data.detail || Object.values(err.response.data)[0];
-  //       setError(detail);
-  //     } else {
-  //       setError("مشکل در اتصال به سرور");
-  //     }
-  //   }
-  // };
-  const UsernameHandler = () => {};
-  const PasswordHandler = (e) => {
-    // if(e.target.value.lenght)
-    let value = e.target.value;
-    setUsername(value);
-    console.log(value.length);
   };
 
   return (
     <div className="flex flex-col items-center justify-center gap-[20px]">
-    {/* Bama Logo */}
+      {/* Bama Logo */}
       <div className="flex-center max-w-[300px]">
         <img src="./Logo/Bama-Login.png" alt="Logo" className="w-full h-full" />
       </div>
@@ -66,7 +36,7 @@ const Login = () => {
           type="text"
           placeholder="نام کاربری"
           value={username}
-          onChange={(e) => PasswordHandler(e)}
+          onChange={(e) => setUsername(e.target.value)}
           className="border-2  border-tint-1 px-[15px] py-[5px] rounded-[10px]"
         />
         <input
