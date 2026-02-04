504b230057dae4e6f96b37e4567fc9015061dc2a
Ali <aliazadigithub@gmail.com>
2026-02-04 10:50:17 +0330

    Add : Register model with API

--- BackEnd-Django/Bama/users/urls.py
+++ BackEnd-Django/Bama/users/urls.py
@@ -1,7 +1,9 @@
 # users/urls.py
 from django.urls import path
 from .views import LoginView
-
+from .views import RegisterAPIView
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
+    path("users/register/", RegisterAPIView.as_view(), name="user-register"),
+
 ]
--- BackEnd-Django/Bama/users/views.py
+++ BackEnd-Django/Bama/users/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import LoginSerializer
 
+from rest_framework import status
+from django.contrib.auth.models import User
+
 class LoginView(APIView):
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
@@ -14,5 +17,32 @@ class LoginView(APIView):
         return Response({
             "refresh": str(refresh),
             "access": str(refresh.access_token),
-            "username": user.username
+            "username": user.username,
+            # -----
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "is_admin": user.is_staff,
         })
+
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        data = request.data
+        username = data.get("username")
+        email = data.get("email")
+        password = data.get("password")
+
+        if not username or not password:
+            return Response({"error": "نام کاربری و رمز عبور الزامی است"}, status=400)
+
+        if User.objects.filter(username=username).exists():
+            return Response({"error": "این نام کاربری قبلاً ثبت شده"}, status=400)
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+        return Response({"message": "ثبت نام با موفقیت انجام شد"}, status=201)
